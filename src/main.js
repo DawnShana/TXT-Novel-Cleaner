fabc81822bc4c5ec199b5ed5abc56471e5ed44bd
SHANA <72056360+DawnShana@users.noreply.github.com>
2026-09-01 19:54:21 +0800

    feat: bind structural learning policy

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import {$$,view,loadRules} from './state.js';
 import {bindCleanFlow} from './clean-flow.js';
 import {bindRulesUI} from './rules-ui.js';
 import {bindGitHub,loadCfg} from './github-sync.js';
+import {bindLearningPolicy} from './learning-policy.js';
 
 async function boot(){
   $$('.tab').forEach(b=>b.onclick=()=>view(b.dataset.view));
-  bindCleanFlow();bindRulesUI();bindGitHub();loadCfg();
+  bindCleanFlow();bindLearningPolicy();bindRulesUI();bindGitHub();loadCfg();
   await loadRules();
   if('serviceWorker'in navigator)navigator.serviceWorker.register('./sw.js').catch(console.warn);
 }
